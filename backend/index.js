77829de0bd63ddded32ccf766cb654191ea0309f
Amulya Reddy <samulya2006@gmail.com>
2025-07-03 14:39:58 +0530

    added routes to index.js file

--- backend/index.js
+++ backend/index.js
@@ -23,6 +23,11 @@ app.use("/media", express.static(path.join(__dirname, "media")));
 app.use("/api/branch", require("./routes/branch.route"));
 app.use("/api/exam", require("./routes/exam.route"));
 app.use("/api/marks", require("./routes/marks.route"));
+app.use("/api/material", require("./routes/material.route"));
+app.use("/api/notice", require("./routes/notice.route"));
+app.use("/api/subject", require("./routes/subject.route"));
+app.use("/api/timetable", require("./routes/timetable.route"));
+
 
 
 app.listen(port, () => {
